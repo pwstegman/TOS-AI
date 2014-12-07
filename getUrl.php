6ad1e0e3491297e8bd3da41228fdad2373d03ee5
Pierce Stegman <pwstegman@gmail.com>
2014-12-07 15:38:58 -0500

    Improved getUrl

--- getUrl.php
+++ getUrl.php
@@ -12,7 +12,7 @@ if(isset($cache[$name]) && time()-$cache[$name]["time"] < 86400){
 	exit();
 }
 
-$googleUrl = "http://www.google.com/search?hl=en&q=".urlencode($name)."+terms&btnI=1";
+$googleUrl = "http://www.google.com/search?hl=en&q=".urlencode($name)."+terms+and+conditions&btnI=1";
 $headers = get_headers($googleUrl, 1);
 $url = $headers['Location'];
 if(is_array($url))
